cc1315a21342bfb3d2708e5fc51adc04917023da
advden4ik <dan12gm@mail.ru>
2019-07-12 17:22:51 +0300

    Added Routing to project

--- src/components/FinishedQuiz/FinishedQuiz.jsx
+++ src/components/FinishedQuiz/FinishedQuiz.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import classes from './FinishedQuiz.module.css';
 import Button from '../UI/Button/Button';
 
@@ -40,12 +41,13 @@ const FinishedQuiz = props => {
                 >
                     Повторить
                 </Button>
-                <Button
-                    onClick={props.onRetry}
-                    type='success'
-                >
-                    Перейти в список тестов
-                </Button>
+                <Link to='/'>
+                    <Button
+                        type='success'
+                    >
+                        Перейти в список тестов
+                    </Button>
+                </Link>
             </div>
         </div>
     );
--- src/components/Navigation/Drawer/Drawer.jsx
+++ src/components/Navigation/Drawer/Drawer.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import classes from './Drawer.module.css';
+import { NavLink } from 'react-router-dom';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const links = [
-    1,2,3
+    {to: '/', label: 'Список', exact: true},
+    {to: '/auth', label: 'Авторизация', exact: false},
+    {to: '/quiz-creator', label: 'Создать тест', exact: false},
 ]
 
 const Drawer = props => {
-
-    const [, ] = React.useState();
 
     const renderLinks = () => {
         return links.map((link, index) => {
             return (
                 <li key={index}>
-                    <a>Link {link}</a>
+                    <NavLink
+                        to={link.to}
+                        exact={link.exact}
+                        activeClassName={classes.active}
+                        onClick={props.onClose}
+                    >
+                        {link.label}
+                    </NavLink>
                 </li>
             );
         });
